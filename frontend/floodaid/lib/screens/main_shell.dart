9c8e143dc87e563c860796e55e7c36827acfb2df
saranyasounder <rajansaranya176@gmail.com>
2026-05-02 15:16:16 -0700

    finished chat

--- frontend/floodaid/lib/screens/main_shell.dart
+++ frontend/floodaid/lib/screens/main_shell.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../chat/chat_screen.dart';
 
 class MainShell extends StatefulWidget {
   const MainShell({super.key});
@@ -12,7 +13,7 @@ class _MainShellState extends State<MainShell> {
 
   final List<Widget> _screens = [
     const Scaffold(body: Center(child: Text('Map coming soon'))),
-    const Scaffold(body: Center(child: Text('Chat coming soon'))),
+    const ChatScreen(),
     const Scaffold(body: Center(child: Text('Resources coming soon'))),
   ];
 
